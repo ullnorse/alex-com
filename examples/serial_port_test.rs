6a95e949c2a42124ac0210335f6e9cd03dbd110f
Aleksa Jonic <ullnorse@gmail.com>
2022-10-31 16:50:49 +0100

    Getting serial data to main thread from serial thread

--- examples/serial_port_test.rs
+++ examples/serial_port_test.rs
@@ -1,13 +1,15 @@
+use std::io;
+
 use serialport::available_ports;
 
 fn main() {
-    let ports = serial2::SerialPort::available_ports().unwrap();
+    // let ports = serial2::SerialPort::available_ports().unwrap();
 
-    for port in ports {
-        println!("{:?}", port);
-    }
+    // for port in ports {
+    //     println!("{:?}", port);
+    // }
 
-    println!("--------------------------------------------------");
+    // println!("--------------------------------------------------");
 
     // let ports: Vec<serialport::SerialPortInfo> = serialport::available_ports().unwrap();
     let ports: Vec<String> =
@@ -21,8 +23,39 @@ fn main() {
         })
         .collect();
 
-    for port in ports {
+    for port in &ports {
         println!("{:?}", port);
+    }
+
+    
+
+    let mut port = serialport::new(ports[0].clone(), 9600)
+        .open()
+        .unwrap();
+
+    port.clear(serialport::ClearBuffer::All).unwrap();
+
+    let mut buffer = [0u8; 1];
+
+    let mut q = Vec::new();
+
+    loop {
+        match port.read_exact(&mut buffer) {
+            Ok(_) => {
+                let c = buffer[0] as char;
+                match c {
+                    '\n' => {
+                        q.push(c);
+                        let s: String = q.iter().collect();
+                        print!("{s}");
+                    },
+                    _ => q.push(c),
+                }
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e)
+        }
+
     }
 
     // let port = serialport::SerialPortBuilder::
